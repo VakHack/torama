e77afd89fd5afcf31c7411cf201e9e865a18b1f1
VakHack <31468467+VakHack@users.noreply.github.com>
2026-07-03 19:39:59 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+/* שלב 1 (המשך): הדבק כאן את קונפיגורציית ה-Firebase שלך          */
+/* ============================================================ */
+const firebaseConfig = {
+  apiKey: "AIzaSyBNOW8D4InzyfdSqHasOgT1zqph9FIoC2o",
+  authDomain: "torama-12b6a.firebaseapp.com",
+  databaseURL: "https://torama-12b6a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "torama-12b6a",
+  storageBucket: "torama-12b6a.firebasestorage.app",
+  messagingSenderId: "587011004925",
+  appId: "1:587011004925:web:a102d103dc07f4febd2194",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+document.getElementById("brandIcon").src = "icon-192.png";
